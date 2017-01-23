2e69422adbed1a602dd5ea213661c288ac941c8c
AlexisHt <alexis.dharcourt@gmail.com>
2017-01-23 18:12:54 +0100

    modif gitignore et readme

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+
+	scope '/admin' do
+		resources :candidates
+	end
+	
+  resources :admin_electeurs
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
