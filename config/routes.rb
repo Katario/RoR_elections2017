916308bdb9956e3769a8f2bd38d47569941eb4e0
Alexandra Angehrn <alexandra.angehrn@eemi.com>
2017-01-24 09:00:14 +0100

    push pour groupe

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
-  get 'log_out' => "sessions#destroy", :as => "log_out"
-  get 'log_in' => "sessions#new", :as => "log_in"
-  get 'sign_up' => "users#new", :as => "sign_up"
-
-
 
   scope '/admin' do
     resources :tours
@@ -13,6 +7,12 @@ Rails.application.routes.draw do
   resources :candidates
   resources :users
   resources :sessions
+
+  get 'sessions/new'
+  get 'log_out' => "sessions#destroy", :as => "log_out"
+  get 'log_in' => "sessions#new", :as => "log_in"
+  get 'sign_up' => "users#new", :as => "sign_up"
+
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
