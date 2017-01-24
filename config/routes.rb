700d2a471a795a85d1e7a9613c9e7f7575bf10eb
Hadrien Boe <hadrien.boe@eemi.com>
2017-01-24 09:39:41 +0100

    scaffold de admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :admins
   get 'users/new'
 
   scope '/admin' do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170123171238) do
+ActiveRecord::Schema.define(version: 20170123172040) do
+
+  create_table "admins", force: :cascade do |t|
+    t.string   "login"
+    t.string   "password"
+    t.boolean  "statut"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "candidates", force: :cascade do |t|
     t.string   "nom"
