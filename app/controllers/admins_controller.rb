3ef7f04f4e00ebb2c41478288a2741e53eeae7f5
Hadrien Boe <hadrien.boe@eemi.com>
2017-01-24 10:03:57 +0100

    changement dans admin_controllers

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -61,6 +61,12 @@ class AdminsController < ApplicationController
     end
   end
 
+  def login
+  end
+
+  def logout
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_admin
