7032969fda3405440d9d11f105d6e08f5fcf2f60
AlexisHt <alexis.dharcourt@gmail.com>
2017-01-26 10:27:16 +0100

    blocage tour

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -18,16 +18,31 @@ class VotesController < ApplicationController
   def create
     is_logged
     @vote = Vote.new(vote_params)
+    @user = current_user
 
     respond_to do |format|
-      if @vote.save
+      if @vote.id_tour == 1
+        @user.tour1 = 1
+        @vote.save
+        @user.save
         format.html { redirect_to @vote, notice: 'Vote was successfully created.' }
-        format.json { render :show, status: :created, location: @vote }
+        format.json { render :show, status: :created, location: @vote }  
+
+      elsif @vote.id_tour == 2
+        @user.tour2 = 1
+        @vote.save
+        @user.save
+        format.html { redirect_to @vote, notice: 'Vote was successfully created.' }
+        format.json { render :show, status: :created, location: @vote }  
+        
       else
         format.html { render :new }
         format.json { render json: @vote.errors, status: :unprocessable_entity }
       end
+
+     
     end
+
   end
 
 
