ee7d9d83c241f07f7f6a37ba617db2725ac6fca9
Hadrien Boe <hadrien.boe@eemi.com>
2017-01-27 11:06:40 +0100

    Modifs

--- app/controllers/admin/votes_controller.rb
+++ app/controllers/admin/votes_controller.rb
@@ -7,16 +7,23 @@ class Admin::VotesController < ApplicationController
     @votes = Vote.all
 	@results = Vote.group(:id_candidate).count
 	@candidates = Candidate.all
+  @isboutonvalided1 = Vote.where(id_tour: 1).select(:vote_validation).first
+  @isboutonvalided2 = Vote.where(id_tour: 2).select(:vote_validation).first
   end
 
   def validation1
-  	 Vote.where('id_tour = 1').update_all(vote_validation: "1")
-  	 redirect_to action: "index"
+  	@vote1 = Vote.where(id_tour: 1)
+
+    if @vote1.update_all(vote_validation: 1)
+  	  redirect_to admin_votes_path
+    end
   end
 
   def validation2
-  	 Vote.where('id_tour = 2').update_all(vote_validation: "2")
-  	 redirect_to action: "index"
-  end
+  	 @vote2 = Vote.where(id_tour: 2)
 
+      if @vote2.update_all(vote_validation: 1)
+        redirect_to admin_votes_path
+      end
+  end
 end
