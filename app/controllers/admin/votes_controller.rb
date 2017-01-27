be2d091f3d66647bd74447f072ed3326434d24bd
AlexisHt <alexis.dharcourt@gmail.com>
2017-01-27 13:47:41 +0100

    correction bug resultats

--- app/controllers/admin/votes_controller.rb
+++ app/controllers/admin/votes_controller.rb
@@ -4,37 +4,16 @@ class Admin::VotesController < ApplicationController
   # GET /votes.json
   def index
     is_admin_logged
-    @votes = Vote.all
-	@results = Vote.group(:id_candidate).count
-	@candidates = Candidate.all
-  @isboutonvalided1 = Vote.where(id_tour: 1).select(:vote_validation).first
-  @isboutonvalided2 = Vote.where(id_tour: 2).select(:vote_validation).first
-  end
+    @votest1 = Vote.where(id_tour: 1)
 
-  def validation1
-
-	  @votest1 = Vote.where(id_tour: 1)
 	  @resultst1 = Vote.where(id_tour: 1).group(:id_candidate).count.sort_by(&:last).reverse
 	  @votest2 = Vote.where(id_tour: 2)
 	  @resultst2 = Vote.where(id_tour: 2).group(:id_candidate).count.sort_by(&:last).reverse.first(2)
 	  @candidates = Candidate.all
 	  @votes = Vote.all
-
-
-    if @votest1.update_all(vote_validation: 1)
-  	  redirect_to admin_votes_path
-    end
   end
 
-
-  def validation2
-  	 @votest2 = Vote.where(id_tour: 2)
-
-      if @votest2.update_all(vote_validation: 1)
-        redirect_to admin_votes_path
-      end
-  end
-
+ 
   def filter
   	@candidates = Candidate.all
   	@votes = Vote.where(code_postal: params[:search]).where(id_tour: params[:tour])
--- db/migrate/20170127113303_add_cols_to_votes.rb
+++
@@ -1,5 +0,0 @@
-class AddColsToVotes < ActiveRecord::Migration[5.0]
-  def change
-    add_column :votes, :vote_validation, :boolean
-  end
-end
